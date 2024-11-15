87251c2e93fa9702c70140662f9c475398a7a4b0
TenebrisUltorem <tenebrisultorem@yandex.ru>
2024-11-15 22:50:56 +0300

    Added tooltips

--- src/main/java/com/github/silveroremod/block/RawSilverBlock.java
+++ src/main/java/com/github/silveroremod/block/RawSilverBlock.java
@@ -2,12 +2,18 @@ package com.github.silveroremod.block;
 
 import com.github.silveroremod.utils.Utils;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+
+import java.util.List;
 
 public class RawSilverBlock extends Block {
 
@@ -23,8 +29,19 @@ public class RawSilverBlock extends Block {
     @Override
     public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
         if (Utils.isUndead(pEntity))
-            pEntity.hurt(pLevel.damageSources().generic(), 0.5F);
+            pEntity.hurt(pLevel.damageSources().generic(), 1.5F);
 
         super.stepOn(pLevel, pPos, pState, pEntity);
+    }
+
+    @Override
+    public void appendHoverText(
+            ItemStack stack,
+            Item.TooltipContext context,
+            List<Component> tooltipComponents,
+            TooltipFlag tooltipFlag
+    ) {
+        tooltipComponents.add(Component.translatable("tooltip.silveroremod.deals_damage_to_undead"));
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
--- src/main/java/com/github/silveroremod/block/SilverOre.java
+++ src/main/java/com/github/silveroremod/block/SilverOre.java
@@ -2,12 +2,18 @@ package com.github.silveroremod.block;
 
 import com.github.silveroremod.utils.Utils;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+
+import java.util.List;
 
 public class SilverOre extends Block {
 
@@ -26,6 +32,17 @@ public class SilverOre extends Block {
             pEntity.hurt(pLevel.damageSources().generic(), 0.5F);
 
         super.stepOn(pLevel, pPos, pState, pEntity);
+    }
+
+    @Override
+    public void appendHoverText(
+            ItemStack stack,
+            Item.TooltipContext context,
+            List<Component> tooltipComponents,
+            TooltipFlag tooltipFlag
+    ) {
+        tooltipComponents.add(Component.translatable("tooltip.silveroremod.deals_minor_damage_to_undead"));
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 
 }
