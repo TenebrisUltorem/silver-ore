ca6ffd815f190b2c7c11871c3837529c6907e90e
TenebrisUltorem <tenebrisultorem@yandex.ru>
2024-11-19 11:06:30 +0300

    Fixed tools's display in hand

--- src/main/java/com/github/silveroremod/item/ItemsRegistry.java
+++ src/main/java/com/github/silveroremod/item/ItemsRegistry.java
@@ -19,12 +19,9 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class ItemsRegistry {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(SilverOreMod.MOD_ID);
 
-    public static final DeferredItem<Item> RAW_SILVER = ITEMS.register(
-            "raw_silver", () -> new Item(new Item.Properties()));
-    public static final DeferredItem<Item> SILVER_INGOT = ITEMS.register(
-            "silver_ingot", () -> new Item(new Item.Properties()));
-    public static final DeferredItem<Item> SILVER_NUGGET = ITEMS.register(
-            "silver_nugget", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> RAW_SILVER = ITEMS.registerSimpleItem("raw_silver");
+    public static final DeferredItem<Item> SILVER_INGOT = ITEMS.registerSimpleItem("silver_ingot");
+    public static final DeferredItem<Item> SILVER_NUGGET = ITEMS.registerSimpleItem("silver_nugget");
 
     public static final DeferredItem<AxeItem> SILVER_AXE = ITEMS.register(
             "silver_axe", SilverAxe::new);
